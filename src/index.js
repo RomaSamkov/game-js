dfd58cf15752381ae100204f314f302216c293ed
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-11 19:49:40 +0200

    add lesson 15 boundaries

--- src/index.js
+++ src/index.js
@@ -87,6 +87,16 @@ window.addEventListener('load', function () {
       this.collisionY += this.speedY * this.speedModifier;
       this.spriteX = this.collisionX - this.width * 0.5;
       this.spriteY = this.collisionY - this.height * 0.5 - 100;
+      // horizontal boundaries
+      if (this.collisionX < this.collisionRadius)
+        this.collisionX = this.collisionRadius;
+      else if (this.collisionX > this.game.width - this.collisionRadius)
+        this.collisionX = this.game.width - this.collisionRadius;
+      // vertical boundaries
+      if (this.collisionY < this.game.topMargin + this.collisionRadius)
+        this.collisionY = this.game.topMargin + this.collisionRadius;
+      else if (this.collisionY > this.game.height - this.collisionRadius)
+        this.collisionY = this.game.height - this.collisionRadius;
       // collision with obstacles
       this.game.obstacles.forEach(obstacle => {
         // [distance < sumOfRadii, distance, sumOfRadii, dx, dy];
@@ -215,7 +225,7 @@ window.addEventListener('load', function () {
             overlap = true;
           }
         });
-        const margin = testObstacle.collisionRadius * 2;
+        const margin = testObstacle.collisionRadius * 3;
         if (
           !overlap &&
           testObstacle.spriteX > 0 &&
