89b1b0d881c0c4207f13d7976eb3f63ea1cc0292
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-11 20:17:39 +0200

    add lesson 16 fps

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ window.addEventListener('load', function () {
       this.dy = 0;
       this.speedModifier = 5;
       this.spriteWidth = 255;
-      this.spriteHeight = 255;
+      this.spriteHeight = 256;
       this.width = this.spriteWidth;
       this.height = this.spriteHeight;
       this.spriteX;
@@ -166,6 +166,9 @@ window.addEventListener('load', function () {
       this.topMargin = 260;
       this.debug = true;
       this.player = new Player(this);
+      this.fps = 70;
+      this.timer = 0;
+      this.interval = 1000 / this.fps;
       this.numberOfObstacles = 8;
       this.obstacles = [];
       this.mouse = {
@@ -195,10 +198,15 @@ window.addEventListener('load', function () {
         console.log(this.debug);
       });
     }
-    render(context) {
-      this.obstacles.forEach(obstacle => obstacle.draw(context));
-      this.player.draw(context);
-      this.player.update();
+    render(context, deltaTime) {
+      if (this.timer > this.interval) {
+        context.clearRect(0, 0, this.width, this.height);
+        this.obstacles.forEach(obstacle => obstacle.draw(context));
+        this.player.draw(context);
+        this.player.update();
+        this.timer = 0;
+      }
+      this.timer += deltaTime;
     }
     checkCollision(a, b) {
       const dx = a.collisionX - b.collisionX;
@@ -244,10 +252,12 @@ window.addEventListener('load', function () {
   game.init();
   console.log(game);
 
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.render(ctx);
+  let lastTime = 0;
+  function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    game.render(ctx, deltaTime);
     requestAnimationFrame(animate);
   }
-  animate();
+  animate(0);
 });
