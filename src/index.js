4a1005b0b8fb1e25b20f7dc138b0251a8f17bb35
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-13 21:27:34 +0200

    add class Enemy

--- src/index.js
+++ src/index.js
@@ -213,6 +213,56 @@ window.addEventListener('load', function () {
     }
   }
 
+  class Enemy {
+    constructor(game) {
+      this.game = game;
+      this.collisionRadius = 30;
+      this.speedX = Math.random() * 3 + 0.5;
+      this.image = document.getElementById('toad');
+      this.spriteWidth = 140;
+      this.spriteHeight = 260;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.collisionX =
+        this.game.width + this.width + Math.random() * this.game.width * 0.5;
+      this.collisionY =
+        this.game.topMargin +
+        Math.random() * (this.game.height - this.game.topMargin);
+      this.spriteX;
+      this.spriteY;
+    }
+    draw(context) {
+      context.drawImage(this.image, this.spriteX, this.spriteY);
+      if (this.game.debug) {
+        context.beginPath();
+        context.arc(
+          this.collisionX,
+          this.collisionY,
+          this.collisionRadius,
+          0,
+          Math.PI * 2
+        );
+        context.save();
+        context.globalAlpha = 0.5;
+        context.fill();
+        context.restore();
+        context.stroke();
+      }
+    }
+    update() {
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height + 40;
+      this.collisionX -= this.speedX;
+      if (this.spriteX + this.width < 0) {
+        this.collisionX =
+          this.game.width + this.width + Math.random() * this.game.width * 0.5;
+        this.collisionY =
+          this.game.topMargin +
+          Math.random() * (this.game.height - this.game.topMargin);
+      }
+    }
+  }
+
   class Game {
     constructor(canvas) {
       this.canvas = canvas;
@@ -230,6 +280,7 @@ window.addEventListener('load', function () {
       this.maxEggs = 10;
       this.obstacles = [];
       this.eggs = [];
+      this.enemies = [];
       this.gameObjects = [];
       this.mouse = {
         x: this.width * 0.5,
@@ -261,7 +312,12 @@ window.addEventListener('load', function () {
     render(context, deltaTime) {
       if (this.timer > this.interval) {
         context.clearRect(0, 0, this.width, this.height);
-        this.gameObjects = [...this.eggs, ...this.obstacles, this.player];
+        this.gameObjects = [
+          this.player,
+          ...this.eggs,
+          ...this.obstacles,
+          ...this.enemies,
+        ];
         this.gameObjects.sort((a, b) => {
           return a.collisionY - b.collisionY;
         });
@@ -292,7 +348,14 @@ window.addEventListener('load', function () {
       this.eggs.push(new Egg(this));
     }
 
+    addEnemy() {
+      this.enemies.push(new Enemy(this));
+    }
+
     init() {
+      for (let i = 0; i < 3; i++) {
+        this.addEnemy();
+      }
       let attempts = 0;
       while (this.obstacles.length < this.numberOfObstacles && attempts < 500) {
         let testObstacle = new Obstacle(this);
